4d5c276a02c5684e4eb9db5c72c6be105d10a2b9
Oleksandr Liabakh <oduvan@Oleksandrs-MacBook-Pro.local>
2015-06-15 21:37:29 +0300

    hf

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ requirements = [l.strip() for l in open(join(source_directory, 'requirements.txt
 
 setup(
     name='checkio_cli',
-    version='0.0.14',
+    version='0.0.15',
     description='CheckiO common line interface for run and debug mission',
     author='CheckiO',
     author_email='igor@checkio.org',
     url='https://github.com/CheckiO/checkio-cli',
-    download_url='https://github.com/CheckiO/checkio-cli/tarball/0.0.14',
+    download_url='https://github.com/CheckiO/checkio-cli/tarball/0.0.15',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['checkio-cli = checkio_cli.cli:main'],
